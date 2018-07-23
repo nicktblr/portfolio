7d33c5abdfa7f76a22f604c5da759d83c4ccc579
Nick <ntabler@ualberta.ca>
2018-07-24 00:17:02 +0100

    Template first page as a Vue component

---
+++ index.js
@@ -0,0 +1,13 @@
+var bodytext = [
+    {text: 'web developer'},
+    {text: '10x coder'},
+    {text: 'graduating in may 2019'}
+];
+
+var content = new Vue({
+    el: '#content',
+    data: {
+        header: 'nick tabler',
+        body: bodytext
+    }
+});
