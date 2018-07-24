d6967b198cf60edd875007bbeb97fea121fdcd5e
Nick <ntabler@ualberta.ca>
2018-07-24 17:54:39 +0100

    Add responsive scrolling

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
-var bodytext = [
+var abouttext = [
     {text: 'web developer'},
     {text: '10x coder'},
     {text: 'graduating in may 2019'}
 ];
 
+var edutext = [
+    {text: 'uofa'},
+    {text: 'electrical engineering'},
+    {text: 'graduating in may 2019'}
+]
+
 var menuOptions ={
     menu_about: {text: 'about me.', selected: true},
-    menu_edu: {text:'education.', selected: false},
-    menu_exp: {text: 'experience.', selected: false},
-    menu_cont: {text: 'contact me.', selected: false}
+    menu_education: {text:'education.', selected: false},
+    menu_experience: {text: 'experience.', selected: false},
+    menu_contact: {text: 'contact me.', selected: false}
 };
 
-var content = new Vue({
-    el: '#content-container',
+var about = new Vue({
+    el: '#about',
     data: {
         header: 'nick tabler',
-        body: bodytext,
-        show: true
+        body: abouttext
     }
 });
+
+var education = new Vue({
+    el: '#education',
+    data: {
+        header: 'education',
+        body: edutext
+    }
+})
+
+var experience = new Vue({
+    el: '#experience',
+    data: {
+        header: 'experience',
+        body: edutext
+    }
+})
+
+var contact = new Vue({
+    el: '#contact',
+    data: {
+        header: 'contact me',
+        body: edutext
+    }
+})
 
 var menu = new Vue({
     el: '#menu',
