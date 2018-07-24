f242ac9235e311341a25b1020e634562048d0f52
Nick <ntabler@ualberta.ca>
2018-07-24 15:55:41 +0100

    Add basic state control to menu

--- index.js
+++ index.js
@@ -4,11 +4,25 @@ var bodytext = [
     {text: 'graduating in may 2019'}
 ];
 
+var menuOptions ={
+    menu_about: {text: 'about me.', selected: true},
+    menu_edu: {text:'education.', selected: false},
+    menu_exp: {text: 'experience.', selected: false},
+    menu_cont: {text: 'contact me.', selected: false}
+};
+
 var content = new Vue({
     el: '#content-container',
     data: {
         header: 'nick tabler',
         body: bodytext,
         show: true
+    }
+});
+
+var menu = new Vue({
+    el: '#menu',
+    data: {
+        options: menuOptions
     }
 });
