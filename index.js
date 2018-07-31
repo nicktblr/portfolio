facdf57a3bc7e8494d14483936545da04beadd4b
Nick <ntabler@ualberta.ca>
2018-07-31 16:06:10 +0100

    Reset to intro page on load

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 
 // Best practice is to use an additional vue component instead of raw html
 
-var abouttext = '<p>engineering student</p>' +
-                '<p>computer programmer</p>' +
-                '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sagittis nec mauris et sagittis. Nulla dictum pellentesque enim in tristique. Proin sed odio a arcu commodo mattis ac vitae sapien. Ut dui lectus, tempus ac augue pulvinar, sagittis dapibus diam. Nunc lobortis sapien id dolor ornare, a aliquam turpis porta. Quisque eu vulputate mauris. Cras vel purus ut erat porttitor dignissim. Aliquam urna nibh, rutrum vitae mattis et, tempor dignissim felis. Sed sed ligula nisl. Aliquam finibus suscipit ligula sed feugiat.</p>'
-
-
 var menuOptions ={
     menu_about: {text: 'about me.', selected: true},
     menu_education: {text:'education.', selected: false},
